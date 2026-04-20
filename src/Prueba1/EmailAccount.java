6ae9534ebfee0ec78c7d37e1ea1e1be2bd6c14a2
Gabriel Zelaya <gabrielzelaya0206@gmail.com>
2026-04-20 15:51:42 -0600

    commit

--- src/Prueba1/EmailAccount.java
+++ src/Prueba1/EmailAccount.java
@@ -45,4 +45,47 @@ public class EmailAccount {
         return false;
     }
     
+    public void mostrarInbox(){
+        int total=0;
+        int sinLeer=0;
+        
+        System.out.println("Cuenta: "+correo+" - "+nombre);
+
+        for(int i =0;i< inbox.length;i++){
+            if(inbox[i] != null){
+                total++;
+
+                String estado =inbox[i].isLeido() ? "LEIDO" : "SIN LEER";
+                    
+                if(!inbox[i].isLeido()){
+                    sinLeer++;
+                }
+
+                System.out.println(i+ " - "+inbox[i].getEmisor()+" - "+inbox[i].getAsunto()+" - "+estado);
+            }
+        }
+        
+        System.out.println("Correos sin leer: "+sinLeer);
+        System.out.println("Total de correos: "+total);
+    }
+
+    public void leerCorreo(int pos){
+        if(pos>= 0 && pos<inbox.length && inbox[pos] !=null){
+            inbox[pos].imprimir();
+            inbox[pos].marcarLeido();
+        }else{
+            System.out.println("Correo No Existe");
+        }
+    }
+
+    public void limpiarInbox(){
+        for(int i = 0; i < inbox.length; i++){
+            if(inbox[i] != null && inbox[i].isLeido()){
+                inbox[i] = null;
+            }
+        }
+    }
+    
 }
+    
+
